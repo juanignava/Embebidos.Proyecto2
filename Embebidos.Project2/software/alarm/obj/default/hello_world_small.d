7877eb18272d7c9a9a41fed6331da4a1fbad07bf
Juan Igancio Navarro <ju.navarro@estudiantec.cr>
2022-09-30 13:34:19 -0600

    Add alarm code
    This has everything ready to be run in the fpga but it is not working when deplyed to the hardware

---
+++ Embebidos.Project2/software/alarm/obj/default/hello_world_small.d
@@ -0,0 +1,9 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ c\:\quartus_embebidos\embebidos.project2\software\alarm_bsp\hal\inc\sys\alt_stdio.h \
+ c\:\quartus_embebidos\embebidos.project2\software\alarm_bsp\hal\inc\alt_types.h \
+ c\:/quartus_embebidos/Embebidos.Project2/software/alarm_bsp/system.h \
+ c\:/quartus_embebidos/Embebidos.Project2/software/alarm_bsp/linker.h
+c\:\quartus_embebidos\embebidos.project2\software\alarm_bsp\hal\inc\sys\alt_stdio.h:
+c\:\quartus_embebidos\embebidos.project2\software\alarm_bsp\hal\inc\alt_types.h:
+c\:/quartus_embebidos/Embebidos.Project2/software/alarm_bsp/system.h:
+c\:/quartus_embebidos/Embebidos.Project2/software/alarm_bsp/linker.h:
